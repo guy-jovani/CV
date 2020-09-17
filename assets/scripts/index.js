4d3b0c23568f4f5eeb997843dd24b957e3323570
Guy Jovani <guy.jovany@mail.huji.ac.il>
2020-09-17 21:10:25 +0300

    - added animation to the header in the index page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,5 @@
-
 import '../styles/emr.css';
 import '../styles/index.css';
-
 
 const toggleNavBarBtn = document.querySelector('.nav-open-collapsed');
 const navLinks = document.querySelector('.nav-closed-links');
@@ -9,184 +7,73 @@ const navLinks = document.querySelector('.nav-closed-links');
 if (toggleNavBarBtn) {
   toggleNavBarBtn.addEventListener('click', () => {
     navLinks.classList.toggle('nav-opened-links');
-    toggleNavBarBtn.firstElementChild.classList.toggle('glyphicon-align-justify');
+    toggleNavBarBtn.firstElementChild.classList.toggle(
+      'glyphicon-align-justify'
+    );
     toggleNavBarBtn.firstElementChild.classList.toggle('glyphicon-remove');
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let lastScrollTop = window.pageYOffset;
-// const experienceSection = document.querySelector('.experience');
-// const skillsSection = document.querySelector('.skills');
-// const sections = document.querySelectorAll('section');
-
-// console.log(sections)
-
-
-
-// const scrollHandler = event => {
-//   let nextVP;
-//   const currVP = Math.floor(window.pageYOffset / window.innerHeight);
-//   if (window.pageYOffset < lastScrollTop) { // up
-//     nextVP = Math.floor((window.pageYOffset + 100) / window.innerHeight);
-//   // console.log(currVP, nextVP)
-//     if (nextVP === currVP) {
-//       // window.scrollTo(0, sections[currVP].offsetTop);
-//       }
-//   } else { // down
-//     nextVP = Math.floor((window.pageYOffset - 100) / window.innerHeight);
-//   // console.log(currVP, nextVP)
-//     if (currVP < sections.length - 1 && nextVP === currVP) {
-//       // window.scrollTo(0, sections[currVP + 1].offsetTop);
-//     }
-//   }
-//   lastScrollTop = window.pageYOffset;
-// };
-
-// document.addEventListener('scroll', scrollHandler);
-
-
-
-
+if (document.querySelector('.profession-wrapper')) {
+  const supportsAnimation =
+    'CSS' in window &&
+    window.innerWidth > 420 &&
+    CSS.supports('clip-path', 'polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)');
+  
+  const firstProfession = document.querySelector('.profession-wrapper')
+    .firstElementChild;
+  let professionActive = document.querySelector('.profession-active');
+
+  const developer = document.querySelector('.developer');
+
+  const animateDeveloper = () => {
+    developer.animate(
+      [
+        { left: '-40px', offset: 0 },
+        { left: '0px', offset: 0.3 },
+        { left: '0px', offset: 0.55 },
+        { left: '-40px', offset: 1 }
+      ],
+      {
+        duration: 3000,
+        iterations: 1
+      }
+    );
+  };
+
+  supportsAnimation ? animateDeveloper() : null;
+
+  const switchActiveProfession = () => {
+    professionActive.classList.replace(
+      'profession-active',
+      'profession-inactive'
+    );
+
+    supportsAnimation ? null : professionActive.animate(
+      [
+        { top: '0', opacity: '1', offset: 0 },
+        { top: '100%', offset: 1 },
+      ],
+      {
+        duration: 2500,
+        iterations: 1
+      }
+    );
+
+    if (professionActive.nextElementSibling) {
+      professionActive = professionActive.nextElementSibling;
+    } else {
+      professionActive = firstProfession;
+    }
+
+    professionActive.classList.replace(
+      'profession-inactive',
+      'profession-active'
+    );
+    supportsAnimation ? animateDeveloper() : null;
+  };
+
+  document
+    .querySelector('.profession-wrapper')
+    .addEventListener('animationend', switchActiveProfession);
+}
